e52f1a75e62a67f1a921669e92a42c0606737612
LeeYunSung <dldbstjd6350@naver.com>
2020-02-25 14:28:35 -0800

    complete

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+//Observer
+public class Inventory : MonoBehaviour, IHasChanged
+{
+    [SerializeField] private Transform slots;
+    [SerializeField] private Text inventoryText;
+    Slot[] slot;
+
+    void Start(){
+        slot = slots.GetComponentsInChildren<Slot>();
+        foreach(Slot slotSlot in slot){
+            slotSlot.Register(this);
+        }
+    }
+    public void HasChanged(){
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+        builder.Append(" - ");
+        foreach (Slot slotSlot in slot){
+            if (slotSlot.item){
+                string name = slotSlot.item.itemName;
+                builder.Append(name);
+                builder.Append(" - ");
+            }
+        }inventoryText.text = builder.ToString();
+    }
+}
+namespace UnityEngine.EventSystems{
+    public interface IHasChanged : IEventSystemHandler{
+        void HasChanged();
+    }
+}
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//Subject 
+public class Slot : MonoBehaviour, IDropHandler
+{
+    public Item item{
+        get{
+            return transform.GetComponentInChildren<Item>();
+        }
+    }
+    Inventory inventory;
+    public void Register(Inventory i) {
+        inventory = i;
+    }
+    public void OnDrop(PointerEventData eventData) {
+        if(!item) {
+            Item.itemBeingDragged.transform.SetParent(transform);
+            if (inventory){
+                inventory.HasChanged();
+            }
+        }
+    }
+}
